efb5ab9141508b03e2f4abddd37e7cd023b87f94
410-39-phurithat <56719@yupparaj.ac.th>
2026-08-04 13:26:06 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
-st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
+st.title("🛒 แอปพลิเคชันคำนวณราคาสินค้ารวม VAT 7%")
 price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
 vat = price * 0.07
 net_price = price - vat
- st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
-st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
+st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): {vat:.2f} บาท")
+st.header(f"• ราคาสุทธิ (รวม VAT): {net_price:.2f} บาท")
 st.divider()
-st.write("นายภูริทัต ถาวร เลขที่ 39  ม.4/10")
+st.write("นายภูริทัต ถาวร เลขที่ 39 ม.4/10")
